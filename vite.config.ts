8402d048c587d9432c8037bb51ca99301fab9770
Nicholas Meyers <nicholas.meyers@hotmail.com>
2025-06-06 00:19:27 +0200

    feat: enable pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,75 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+          registerType: 'autoUpdate',
+          manifest: {
+              name: 'Head Of TP',
+              short_name: 'Head Of TP',
+              description: 'Head Of TP',
+              theme_color: '#454D5A',
+              icons: [
+                  {
+                      "src": "icon-48x48.png",
+                      "sizes": "48x48",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "icon-72x72.png",
+                      "sizes": "72x72",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "icon-96x96.png",
+                      "sizes": "96x96",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "icon-128x128.png",
+                      "sizes": "128x128",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "icon-144x144.png",
+                      "sizes": "144x144",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "icon-152x152.png",
+                      "sizes": "152x152",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "icon-192x192.png",
+                      "sizes": "192x192",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "icon-256x256.png",
+                      "sizes": "256x256",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "icon-384x384.png",
+                      "sizes": "384x384",
+                      "type": "image/png"
+                  },
+                  {
+                      "src": "icon-512x512.png",
+                      "sizes": "512x512",
+                      "type": "image/png"
+                  }
+              ]
+          }
+      })
+  ],
   define: {
     global: 'window',
   },
