2e40d9997841ccc360df8740596c8e3ced39667d
Nicholas Meyers <nicholas.meyers@hotmail.comxit>
2025-07-09 00:04:53 +0200

    chore: update service worker

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,23 @@ import Navbar from "./components/navbar";
 import DashboardPage from "./pages/dashboard/DashboardPage.tsx";
 import InfoPage from "./pages/info";
 
+import { useEffect } from 'react';
+import { registerSW } from 'virtual:pwa-register';
+
 function App() {
+    useEffect(() => {
+        const updateSW = registerSW({
+            onNeedRefresh() {
+                if (confirm('A new version is available. Do you want to reload?')) {
+                    updateSW(true);
+                }
+            },
+            onOfflineReady() {
+                console.log('App is klaar voor offline gebruik');
+            },
+        });
+    }, []);
+
     return (
         <div className="min-h-screen bg-gray-50">
             <Navbar />
