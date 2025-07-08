02c713a1c028930bf3cedb3eb3613fc333b8792e
Nicholas Meyers <nicholas.meyers@hotmail.comxit>
2025-07-08 23:50:05 +0200

    chore: update vite pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       tailwindcss(),
       VitePWA({
           registerType: 'autoUpdate',
+          injectRegister: 'auto',
           manifest: {
               name: 'Head Of TP',
               short_name: 'Head Of TP',
